1f62095e87f381fb24de71210f1d2ad92767f3eb
Alexey <bondpuoq@users.noreply.github.com>
2016-09-24 00:47:28 +0500

    Fix double brackets replacing

--- dest/js/allScripts.js
+++ dest/js/allScripts.js
@@ -14801,7 +14801,7 @@ function Preview() {
     var i = 0, urlArray;
     urlArray = rawUrlString.split(",")
       .map(function(current) {
-        return {number: i++, src: current.trim().replace("\"","")}
+        return {number: i++, src: current.trim().replace(/"/g,'')}
       });
     return urlArray;
     };
